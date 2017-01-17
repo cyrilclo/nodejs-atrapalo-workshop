efd8027d835d163ea4bfd20829e57c4025a1deec
Cyril  <cyril.clochard@atrapalo.com>
2017-01-17 18:43:02 +0100

    wip1

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 let express = require('express');
+let mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test_books');
+let Book = require('./models/book');
+
+let caca = new Book('caca', 'caca', 'author', 'caca picture');
+
+//import { Book } from 'models/book';
+
+//let book = new Book('caca', 'caca', 'author', 'caca picture');
+
+console.log(caca);
+
+caca.mongoo
